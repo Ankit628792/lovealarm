747e15db1aad422fd4adfc47403cfb3add9686ec
Ankit Kumar <ankit@sugoilabs.com>
2023-07-26 10:23:22 +0530

    linting rm

---
+++ src/pages/500.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const metadata = {
+    title: 'Love 2.0 | Internal server error',
+    description: 'Looks like that love alarm 2.0 server got some issue, try again again some time it will be available soon.'
+}
+
+function Error() {
+    return (
+        <>
+            <section className='flex flex-col justify-between w-full flex-grow'>
+                <img src='/500.svg' className='max-h-[500px] mx-5 my-20 sm:m-16' alt='internal server error' />
+            </section>
+        </>
+    )
+}
+
+export default Error
