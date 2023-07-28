c66a87cccf4fab1e9a600129c81d97589d37ff5e
Ankit Kumar <ankit@sugoilabs.com>
2023-07-28 23:08:44 +0530

    delnki meta

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -12,13 +12,16 @@ export async function GET() {
 }
 export async function POST(request) {
 
-    let { name, email, message, mobile } = await request.json();
+    try {
 
-    let data = {
-        email: 'ankit628792@gmail.com',
-        subject: `${name} has contacted you on love alarm 2.0`,
-        text: `${message}`,
-        html: `
+
+        let { name, email, message, mobile } = await request.json();
+
+        let data = {
+            email: 'ankit628792@gmail.com',
+            subject: `${name} has contacted you on love alarm 2.0`,
+            text: `${message}`,
+            html: `
             <div style="
             padding: 20px 30px;
         "><h1 style="
@@ -40,17 +43,17 @@ export async function POST(request) {
         ">Regards<br>${name}<br>email: ${email} <br> mobile number: ${mobile || 'None'}</p>
         </div>
             `
-    }
+        }
 
-    sendEMail(data);
+        sendEMail(data);
 
-    if (email) {
+        if (email) {
 
-        let data2 = {
-            email: email,
-            subject: `Thanks for contacting Love Alarm 2.0`,
-            text: `${message}`,
-            html: `
+            let data2 = {
+                email: email,
+                subject: `Thanks for contacting Love Alarm 2.0`,
+                text: `${message}`,
+                html: `
             <div style="
             padding: 20px 30px;
         "><h1 style="
@@ -69,15 +72,19 @@ export async function POST(request) {
             font-size: 16px;
         ">Thanks & Regards,<br/>Team Love 2.0</p></div>
             `
+            }
+            await sendEMail(data2);
         }
-        await sendEMail(data2);
-    }
 
-    let json_response = {
-        success: true,
-        message: 'Mail sent',
-    };
-    return NextResponse.json(json_response)
+        let json_response = {
+            success: true,
+            message: 'Mail sent',
+        };
+        return NextResponse.json(json_response)
+
+    } catch (error) {
+        return NextResponse.send({ error })
+    }
 }
 
 
