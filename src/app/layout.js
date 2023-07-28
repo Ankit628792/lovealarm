8b1ca7bf270cb549e6151f1da30fd1c83f81516d
Ankit Kumar <ankit@sugoilabs.com>
2023-07-28 18:15:35 +0530

    tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,34 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KCSHVFK0DY"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-KCSHVFK0DY');
+          `,
+          }}
+        />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-52W2BTR3');
+          `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
@@ -28,6 +56,12 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       <body className={`flex flex-col w-full min-h-screen`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52W2BTR3"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        {/* <End Google Tag Manager (noscript) */}
         <Navbar />
         {children}
         <Footer />
