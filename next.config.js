dd0b92b711eff13084a52803ae68e1babd59eac6
Ankit Kumar <ankit@sugoilabs.com>
2023-08-13 18:06:07 +0530

    seo

--- next.config.js
+++ next.config.js
@@ -14,5 +14,24 @@ module.exports = {
         mail_user: process.env.MAIL_USER,
         mail_password: process.env.MAIL_PASSWORD,
         contact_url: process.env.CONTACT_URL,
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/sitemap',
+                destination: '/sitemap.xml',
+                permanent: true,
+            },
+            {
+                source: '/rss',
+                destination: '/rss.xml',
+                permanent: true,
+            },
+            {
+                source: '/robots',
+                destination: '/robots.txt',
+                permanent: true,
+            },
+        ]
+    },
 }
