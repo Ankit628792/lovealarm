73d3b7d718e8b85327e61b3ec76052b5ffdb75ba
Ankit Kumar <ankit@sugoilabs.com>
2023-11-16 19:58:33 +0530

    shippin

---
+++ src/app/shipping/page.js
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import React from 'react'
+
+
+function shipping() {
+    return (
+        <>
+            <Head>
+                <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"></meta>
+                <meta name='keywords' content='love alarm 2.0 terms and conditions, terms ,love alarm, love alarm 2.0 , love alarm app 2.0, love alarm policy, love 2.0, 사랑알람 2.0 , 사랑알람 , 사랑알람 2.0 앱, 사랑알람 앱, love alarm download, love alarm apk, love alarm limitation, ankit62879, love alarm ios, love alarm version 2 ios, delanki, product company, product development company, love alarm website , love alarm 2.0 website, love alarm 2.o website, 러브알람 2.0 홈페이지, 러브알람 홈페이지,, love alarm 2.o' />
+            </Head>
+            <section className='flex flex-col gap-10 justify-between min-h-screen w-full' dangerouslySetInnerHTML={{__html:
+            `<html>
+            <head>
+                <!-- Responsive meta tag -->
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <style>
+                    body { margin: 0; }
+                    .header-conatiner { display: flex; align-items: center; padding: 24px; }
+                    .header-text { font-size: 18px; line-height: 24px; color: #fff; margin: 0 0 0 18px; }
+                    .compalinace-page { background: #E63264; font-family: Lato, sans-serif; font-style: normal; font-weight: 400; }
+                    @media screen and (min-width: 768px) { .content-container { margin: 0 auto; max-width: 1000px; } .header-conatiner { margin: 0 auto; max-width: 1000px; } }
+                    .compalinace-content { background: #ffffff; border-radius: 32px 32px 0 0; padding: 32px 24px; }
+                    .content-head { font-size: 24px; line-height: 30px; color: #213554; margin: 0; font-weight: 700;}
+                    .content-seprater { width: 28px; height: 5px; background-color: #213554; margin-top: 16px; }
+                    .updated-date { margin: 16px 0 0; color: #213554ab; font-weight: 700;}
+                    .content-text { color: #515978; margin: 16px 0 0; }
+                    .content-text { font-size: 14px; line-height: 20px; }
+                    .merchant-logo { width: 74.6px; height: 64px; background: #fefefe; box-shadow: 0px 0px 8px #00000040; border-radius: 8px; display: flex; justify-content: center; align-items: center; }
+                    @media screen and (max-width: 330px) { .merchant-logo { width: 87px; } }
+                    @media all and (min-width: 768px), (min-width: 383px) { .merchant-logo { width: 64px; } }
+                    .logo-container { display: flex; justify-content: center; align-items: center; width: 50px; height: 50px; background: #E63264; box-shadow: 11.0003px 22.0007px 53.9016px rgba(0, 0, 0, 0.1), 22.0007px 11.0003px 53.9854px -2.03719px rgba(255, 255, 255, 0.1); border-radius: 50%; }
+                    .logo-text {color: #fff; font-weight: 700; font-size: 32px; }
+                    .list-item { display: list-item; padding-left: 5px; }
+                    .unorder-list { margin: 0; }
+                    .list-text { margin-top: 8px; }
+                </style>
+                        <style>
+                        .content-head { font-size: 20px; }
+                    </style>
+                    </head>
+            <div class= 'compalinace-page'>
+                        <div class= 'header-conatiner'>
+                                    <div class= 'merchant-logo'>
+                            <div class= 'logo-container'><div class= 'logo-text'>A</div></div>
+                        </div>
+                                                    <p class= 'header-text'>ANKIT KUMAR</p>
+                    </div>
+                        <div class='compalinace-content'>
+                                                                            <div class='content-container'>
+                <p class='content-head'>Shipping &amp; Delivery Policy</p>
+                <div class='content-seprater'></div>
+                <p class='updated-date'>Last updated on Nov 16th 2023</p>
+            
+                
+                        <p class='content-text'>Shipping is not applicable for business.</p>
+                </div>
+                                 </div>
+            </div>
+            </html>`}}>
+                
+            </section>
+        </>
+    )
+}
+
+export default shipping
